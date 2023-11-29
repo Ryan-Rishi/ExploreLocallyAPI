d5cee344948313798947d24f4d4a62d17eb3b5d1
Rishi, Ryan Milan <rrishi2024@yahoo.com>
2023-11-29 15:58:58 -0500

    push

--- api.py
+++ api.py
@@ -349,4 +349,4 @@ def get_advisors():
         return Response(response=json.dumps({"error": str(e)}), content_type='application/json', status=500)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
